73422a2a06b47bd9ea0e4d974ed08e39aec87bcf
UlumBahrul <203040089@mail.unpas.ac.id>
2022-10-07 16:09:19 +0700

    belajar constructor

---
+++ oop/constructor.php
@@ -0,0 +1,30 @@
+<?php
+
+use Produk as GlobalProduk;
+
+class Produk
+{
+    public $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0;
+
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis,$this->penerbit";
+    }
+}
+
+
+$produk1 = new Produk("Naruto", "Masashi Kisioto", "Shonen jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+
+
+echo "Komik :" . $produk1->getLabel();
+echo "<br>";
+echo "Game :" . $produk2->getLabel();
