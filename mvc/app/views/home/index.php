0fe44c92aebdd0cb67cdba4d8585f86964e25ebf
UlumBahrul <203040089@mail.unpas.ac.id>
2022-10-14 19:13:16 +0700

    menambahkan asset

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,2 +1,9 @@
-
-    <h1>Selamat Datang</h1>
+<div class="container">
+    <div class="card mt-4">
+        <div class="card-body">
+            <h5 class="card-title">Selamat Datang di Website saya!</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
